bc90dceeb5654afb8d5bb1523f068312deb8202a
Pheema <Pheema@users.noreply.github.com>
2018-08-16 23:42:35 +0900

    slnの複数構成対応

--- conan_install.bat
+++ conan_install.bat
@@ -3,7 +3,8 @@ if not exist Build (
 )
 
 pushd Build
-conan install .. -g visual_studio_multi -s arch=x86_64 -s build_type=Debug -s compiler.runtime=MTd
-conan install .. -g visual_studio_multi -s arch=x86_64 -s build_type=Release -s compiler.runtime=MT
+conan install .. -s arch=x86_64 -s build_type=Debug -s compiler.runtime=MTd --build=missing
+conan install .. -s arch=x86_64 -s build_type=RelWithDebInfo -s compiler.runtime=MT --build=missing
+conan install .. -s arch=x86_64 -s build_type=Release -s compiler.runtime=MT --build=missing
 cmake .. -G "Visual Studio 15 2017 Win64"
 popd
