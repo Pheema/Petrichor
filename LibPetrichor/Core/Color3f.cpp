1f97ed50365fffe07e2123b1492dbe942911c47f
Pheema <Pheema@users.noreply.github.com>
2018-08-26 22:42:36 +0900

    gccでのエラーを修正

--- LibPetrichor/Core/Color3f.cpp
+++ LibPetrichor/Core/Color3f.cpp
@@ -4,7 +4,7 @@ namespace Petrichor
 {
 
 float
-Petrichor::GetLuminance(const Color3f color3f)
+GetLuminance(const Color3f color3f)
 {
     // #TODO: とりあえず。
     return 0.2126f * color3f.x + 0.7152f * color3f.y + 0.0722f * color3f.z;
