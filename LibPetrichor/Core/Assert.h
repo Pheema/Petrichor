5097a4f2bee62448013138508eb65d50b572624c
Pheema <Pheema@users.noreply.github.com>
2018-08-29 00:01:39 +0900

    Fix: RelWithDebugInfoでもASSERTが使用できるように

--- LibPetrichor/Core/Assert.h
+++ LibPetrichor/Core/Assert.h
@@ -1,17 +1,26 @@
 ﻿#pragma once
 
 #include "Constants.h"
-#include <cassert>
 
 #ifdef PHM_DEBUG
 
-#define ASSERT(x) assert(x)
-#define UNIMPLEMENTED() assert(false && "This function is unimplemented")
+#define ASSERT(expression)                                                     \
+    if (expression)                                                            \
+    {                                                                          \
+        (void*)0;                                                              \
+    }                                                                          \
+    else                                                                       \
+    {                                                                          \
+        int* tmp = nullptr;                                                    \
+        *tmp = 0;                                                              \
+    }
+
+#define UNIMPLEMENTED() ASSERT(false && "This function is unimplemented")
 #define IS_NORMALIZED(v)                                                       \
-    assert(Petrichor::Math::ApproxEq((v).Length(), 1.0f, Petrichor::kEps) &&   \
+    ASSERT(Petrichor::Math::ApproxEq((v).Length(), 1.0f, Petrichor::kEps) &&   \
            "'#v' is not normalized.")
 #define IS_APPROXEQ(x, y)                                                      \
-    assert(Petrichor::Math::ApproxEq((x), (y), Petrichor::kEps) &&             \
+    ASSERT(Petrichor::Math::ApproxEq((x), (y), Petrichor::kEps) &&             \
            "'#x' is not equal to '#y'.")
 
 #else
