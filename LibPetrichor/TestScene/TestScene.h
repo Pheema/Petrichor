6fd2e969924ecb1273e5d097c44632abdd8440da
Pheema <Pheema@users.noreply.github.com>
2018-09-05 03:05:51 +0900

    実装をcpp側に移動

--- LibPetrichor/TestScene/TestScene.h
+++ LibPetrichor/TestScene/TestScene.h
@@ -10,63 +10,7 @@ namespace Core
 {
 
 void
-LoadCornellBoxScene(Scene* scene)
-{
-    if (scene == nullptr)
-    {
-        return;
-    }
-
-    scene->LoadSceneSettings();
-
-    const auto* matLambertRed = new Lambert(Color3f(1.0f, 0, 0));
-    const auto* matLambertGreen = new Lambert(Color3f(0, 1.0f, 0));
-    const auto* matLamberWhite = new Lambert(Color3f::One());
-    const auto* matEmissionWhite = new Emission(Color3f::One());
-
-    auto const leftWall = new Mesh();
-    leftWall->Load("Resource/SampleScene/CornellBox/LeftWall.obj",
-                   matLambertRed,
-                   ShadingTypes::Flat);
-
-    auto const rightWall = new Mesh();
-    rightWall->Load("Resource/SampleScene/CornellBox/RightWall.obj",
-                    matLambertGreen,
-                    ShadingTypes::Flat);
-
-    auto const whiteWall = new Mesh();
-    whiteWall->Load("Resource/SampleScene/CornellBox/WhiteWall.obj",
-                    matLamberWhite,
-                    ShadingTypes::Flat);
-
-    auto const whiteBox = new Mesh();
-    whiteBox->Load("Resource/SampleScene/CornellBox/WhiteBox.obj",
-                   matLamberWhite,
-                   ShadingTypes::Flat);
-
-    auto const ceilLight = new Mesh();
-    ceilLight->Load("Resource/SampleScene/CornellBox/CeilLight.obj",
-                    matEmissionWhite,
-                    ShadingTypes::Flat);
-
-    scene->AppendMesh(*leftWall);
-    scene->AppendMesh(*rightWall);
-    scene->AppendMesh(*whiteWall);
-    scene->AppendMesh(*whiteBox);
-    scene->AppendLightMesh(*ceilLight);
-
-    auto const camera =
-      new Camera(Math::Vector3f(0, -6.0f, 0), Math::Vector3f::UnitY());
-
-    camera->FocusTo(Math::Vector3f::Zero());
-
-    scene->SetMainCamera(*camera);
-
-    // レンダリング先を指定
-    auto targetTex = new Texture2D(scene->GetSceneSettings().outputWidth,
-                                   scene->GetSceneSettings().outputHeight);
-    scene->SetTargetTexture(targetTex);
-}
+LoadCornellBoxScene(Scene* scene);
 
 } // namespace Core
 } // namespace Petrichor
