84022a9f191b321f76c9887e533a54cc7c3a3984
Pheema <Pheema@users.noreply.github.com>
2019-09-13 23:38:49 +0900

    stbをconan管理からgit submodule管理に変更

--- LibPetrichor/Core/Texture2D.cpp
+++ LibPetrichor/Core/Texture2D.cpp
@@ -1,9 +1,9 @@
 #include "Texture2D.h"
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include "stb/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
+#include "stb/stb_image_write.h"
 
 namespace Petrichor
 {
